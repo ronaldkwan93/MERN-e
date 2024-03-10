b00302374d30d5307609472cead67d78ca28ac13
Ronald Kwan <ronaldkwan93@gmail.com>
2024-03-11 00:37:42 +1100

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,16 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log('Connected to MongoDB!');
+    }).catch((err) => {
+    console.log(err);
+    });
 
 const app = express();
 
-app.listen(4000, () => {
-    console.log("Server is running on 3000!!");
+app.listen(6000, () => {
+    console.log("Server is running on 6000!!");
 });
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,3 +1,4 @@
+import { FaSearch } from 'react-icons/fa'
 import { Link } from "react-router-dom"
 
 export default function Header() {
@@ -10,18 +11,19 @@ export default function Header() {
             <span className='text-slate-700'>Estate</span>
         </h1>
         </Link>
-        <form action="" className='bg-slate-100 p-3 rounded-lg'>
-            <input type="text" placeholder='Search...' className='bg-transparent focus:outline-none'/>
+        <form action="" className='bg-slate-100 p-3 rounded-lg flex items-start'>
+            <input type="text" placeholder='Search...' className='bg-transparent focus:outline-none w-24 sm:w-64'/>
+            <FaSearch className='text-slate-600'/>
         </form>
         <ul className="flex gap-4">
             <Link to="/">
-            <li>Home</li>
+            <li className='hidden sm:inline text-slate-700 hover:underline'>Home</li>
             </Link>
             <Link to="/about">
-            <li>About</li>
+            <li className='hidden sm:inline text-slate-700 hover:underline'>About</li>
             </Link>
             <Link to="/sign-in">
-            <li>Sign In</li>
+            <li className='sm:inline text-slate-700 hover:underline'>Sign In</li>
             </Link>
         </ul>
         </div>
